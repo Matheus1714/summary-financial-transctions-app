d0e10558389e76f3e87a76ee3b1f99e5bdca786a
matheus1714 <matheussilvamartins1714@gmail.com>
2023-09-01 15:37:54 -0300

    remove particles configurations

--- src/components/ParticlesBackground/index.tsx
+++ src/components/ParticlesBackground/index.tsx
@@ -1,6 +1,7 @@
-import Particles from 'react-particles'
-import { particleConfig } from './particle.config'
+// import Particles from 'react-particles'
+// import { particleConfig } from './particle.config'
 
 export function ParticlesBackground() {
-  return <Particles params={particleConfig} />
+  // return <Particles params={particleConfig} />
+  return <h1>Particles</h1>
 }
