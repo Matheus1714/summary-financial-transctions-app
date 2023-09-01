704c37edc073fde1138812ba437418daf3b98bea
matheus1714 <matheussilvamartins1714@gmail.com>
2023-09-01 19:28:57 -0300

    add transaction router

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom'
 import { Home } from './pages/Home'
 import { Loading } from './pages/Loading'
 import { Introduction } from './pages/Introduction'
+import { Transactions } from './pages/Transactions'
 
 export function Router() {
   return (
@@ -9,6 +10,7 @@ export function Router() {
       <Route path="/" element={<Home />} />
       <Route path="/loading" element={<Loading />} />
       <Route path="/introduction" element={<Introduction />} />
+      <Route path="/transactions" element={<Transactions />} />
     </Routes>
   )
 }
