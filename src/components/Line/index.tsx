08b6b2b2396c5ba62fda2582ca9887e6b7c0520f
matheus1714 <matheussilvamartins1714@gmail.com>
2023-09-03 01:28:16 -0300

    create line component

---
+++ src/components/Line/index.tsx
@@ -0,0 +1,5 @@
+import * as S from './styled'
+
+export function Line() {
+  return <S.Container />
+}
---
+++ src/components/Line/styled.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  height: 0.5rem;
+  width: 100%;
+
+  background: ${(props) => props.theme.purpleNormal};
+
+  border-radius: 9999px;
+`
